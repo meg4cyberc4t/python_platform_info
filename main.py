a988ffa81d2568dbf46b70b555823f8153ab7acf
meg4cyberc4t <66907532+meg4cyberc4t@users.noreply.github.com>
2022-06-06 11:14:59 +0300

    Add shutil & psutil

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 import platform
 import cpuinfo
+import shutil
+import psutil
 from pprint import pprint
 
+print('About processor:')
 print(platform.platform())
 print(platform.processor())
 print(platform.python_build())
@@ -11,18 +14,15 @@ print(platform.python_version())
 print(platform.release())
 pprint(cpuinfo.get_cpu_info())
 
-# macOS-12.4-arm64-arm-64bit
-# arm
-# ('default', 'May 17 2022 12:55:41')
-# Clang 13.1.6 (clang-1316.0.21.2.5)
-# CPython
-# 3.8.9
-# 21.5.0
-# {'arch': 'ARM_8',
-#  'arch_string_raw': 'arm64',
-#  'bits': 64,
-#  'brand_raw': 'Apple M1',
-#  'count': 8,
-#  'cpuinfo_version': [8, 0, 0],
-#  'cpuinfo_version_string': '8.0.0',
-#  'python_version': '3.8.9.final.0 (64 bit)'}
+print()
+
+print('About disk:')
+total, used, free = shutil.disk_usage("/")
+print("Total: %d GiB" % (total // (2**30)))
+print("Used: %d GiB" % (used // (2**30)))
+print("Free: %d GiB" % (free // (2**30)))
+
+print()
+
+print("About RAM:")
+print(psutil.virtual_memory())
